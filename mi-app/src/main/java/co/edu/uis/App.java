716843c7483bace2a31333b0c307c260651f7ba4
ga-vo <0vega.gabriel@gmail.com>
2021-11-16 10:57:58 -0500

    add some features: a happy smile :)

--- mi-app/src/main/java/co/edu/uis/App.java
+++ mi-app/src/main/java/co/edu/uis/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Equipo pamplona!" );
+        System.out.println( "Equipo pamplona! :)" );
     }
 }
